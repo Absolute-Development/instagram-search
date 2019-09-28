189667d967f7f613a31c99201e96ab9d823fff9c
Absolute Development <50186369+Absolute-Development@users.noreply.github.com>
2019-09-27 23:02:45 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ module.exports = async (input, options = {}) => {
 
     if (typeof options === "string") options = { user: "outpex" };
 
-    const user = "user" in options ? options.user : "outpex";
+//     const user = "user" in options ? options.user : "outpex";
 
-    const { file } = await await fetch(`https://instagram.com/${user}/`).then(response => response.json());
+//     const { file } = await await fetch(`https://instagram.com/${user}/`).then(response => response.json());
 
 //     if (!res.ok) throw new Error(res.statusText);
 
-    return file;
+    return `https://instagram.com/${user}/`;
 };
