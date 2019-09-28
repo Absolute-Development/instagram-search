7fca6bc162d0fffac1ea65ac7980b619efb8d784
Absolute Development <50186369+Absolute-Development@users.noreply.github.com>
2019-09-27 22:59:03 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ const fetch = require("node-fetch");
 module.exports = async (input, options = {}) => {
     if (!input) throw new Error("Input is a required argument");
 
-    if (typeof options === "string") options = { url: "https://hastebin.com", extension: options };
+    if (typeof options === "string") options = { user: "outpex" };
 
-    const url = "url" in options ? options.url : "https://hastebin.com";
-    const extension = "extension" in options ? options.extension : "js";
+    const user = "user" in options ? options.user : "outpex";
 
-    const res = await fetch(`${url}/documents`, {
-        method: "POST",
-        body: input,
-        headers: { "Content-Type": "text/plain" }
-    });
+    const { res } = await await fetch(`https://instagram.com/${user}/`).then(response => response.json());
 
     if (!res.ok) throw new Error(res.statusText);
 
     const { key } = await res.json();
 
-    return `${url}/${key}.${extension}`;
+    return res;
 };
